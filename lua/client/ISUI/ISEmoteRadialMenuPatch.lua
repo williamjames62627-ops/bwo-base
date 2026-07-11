83ecc6fc89b961bcb692abc2a305ed954544e83c
williamjames62627-ops <williamjames62627@gmail.com>
2026-07-11 16:09:06 -0700

    Delete lua directory

--- lua/client/ISUI/ISEmoteRadialMenuPatch.lua
+++
@@ -1,9 +0,0 @@
-require "ISUI/ISEmoteRadialMenu"
-
-
-local actionEmote = ISEmoteRadialMenu.emote
-
-function ISEmoteRadialMenu:emote(emote)
-    actionEmote(self, emote)
-    triggerEvent("OnEmote", self.character, emote)
-end
