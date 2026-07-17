8a8dc980dc9848caff5587cab10e263c4b5586c9
williamjames62627-ops <williamjames62627@gmail.com>
2026-07-16 20:23:35 -0700

    Delete lua directory

--- lua/client/TimedActions/ISFitnessActionPatch.lua
+++
@@ -1,9 +0,0 @@
-require "TimedActions/ISFitnessAction"
-
-local perform = ISFitnessAction.exeLooped
-
-function ISFitnessAction:exeLooped()
-	perform(self)
-    triggerEvent("OnFitnessActionExeLooped", self)
-end
-
--- lua/client/TimedActions/ISInventoryTransferActionPatch.lua
+++
@@ -1,9 +0,0 @@
-require "TimedActions/ISInventoryTransferAction"
-
-local perform = ISInventoryTransferAction.perform
-
-function ISInventoryTransferAction:perform()
-    perform(self)
-    triggerEvent("OnInventoryTransferActionPerform", self)
-end
-
